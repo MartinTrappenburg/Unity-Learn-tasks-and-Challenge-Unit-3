1de3212d58b2442152cf4166952cf99a3d7e9b69
LAPTOP-0TH2B7ID\marti <330832@student.mboutrecht.nl>
2021-01-29 11:37:09 +0100

    FINALLY FIXED IT.
    INITIAL COMMIT. LOST 3 YEARS OF MY LIFE DUE TO THE INSANITY FROM THE PAST HOUR. READY TO SMASH LAPTOP BUT IT WORKS.
    ALL FILES FOR UNITY LEARN UNIT 3(THE TASKS AND THE CHALLENGE) WILL BE UPLOADED HERE.

---
+++ Unity Unit 3 Tasks/Assets/scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce;
+    public float gravityModifier;
+    private Rigidbody playerRb;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+            isOnGround = false;
+            dirtParticle.Stop();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            dirtParticle.Play();
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+            dirtParticle.Stop();
+            explosionParticle.Play();
+            gameOver = true;
+            Debug.Log("Game Over");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+        }
+        playerAnim.SetTrigger("Jump_trig");
+    }
+}
